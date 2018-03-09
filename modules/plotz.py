20b875f1a7cbd44472e3edc4c7af74ca96ef7866
berceanu <andrei.berceanu@eli-np.ro>
2018-03-09 19:18:42 +0200

    completed Plot2D class

--- modules/plotz.py
+++ modules/plotz.py
@@ -9,13 +9,20 @@ from matplotlib.gridspec import GridSpec
 class Plot2D:
     'Pseudocolor plot of a 2D array with optional 1D slices attached.'
 
-    def __init__(self, arr2d, h_axis, v_axis, **param):
+    def __init__(self, arr2d, h_axis, v_axis, xlabel=r'', ylabel=r'', zlabel=r''):
+        r"""
+        >>> uu = np.linspace(0, np.pi, 128)
+        >>> data = np.cos(uu - 0.5) * np.cos(uu.reshape(-1, 1) - 1.0)
+        >>> p2d = Plot2D(data, uu, uu, xlabel=r'$x$ ($\mu$m)',
+                ylabel=r'$y$ ($\mu$m)', zlabel=r'$\rho$ (cm$^{-3}$)')
+        """
         self.data = arr2d
         self.h_axis = h_axis
         self.v_axis = v_axis
-        self.param = param
+        self.label = {'x':xlabel, 'y':ylabel, 'z':zlabel}
         self.extent = [np.min(h_axis), np.max(h_axis),
                        np.min(v_axis), np.max(v_axis)]
+        self.param = {'figsize':(6.4, 6.4), 'cmap':'coolwarm'}
 
 
     @staticmethod
@@ -33,115 +40,109 @@ class Plot2D:
         return fig.colorbar(mappable, cax=cax)
 
 
-    def cbar(self, ax):
+    def cbar(self, ax=None, **kwargs):
         r"""
-        >>> uu = np.linspace(0, np.pi, 128)
-        >>> data = np.cos(uu - 0.5) * np.cos(uu.reshape(-1, 1) - 1.0)
-        >>> plot2d(ax, data, uu, uu, {'cmap':'coolwarm',
-                              'xlabel':r'$x$ ($\mu$m)',
-                              'ylabel':r'$y$ ($\mu$m)',
-                              'zlabel':r'$\rho$ (cm$^{-3}$)',
-                              'cbar':True})
+        >>> p2d.cbar(figsize=(4.8, 4.8))
         """
-        fig, ax = plt.subplots(figsize=self.param['figsize'])  ##(4.8, 4.8)
-        im = ax.imshow(data, origin='lower',
+        if not ax:
+            self.param.update(kwargs)
+            fig, ax = plt.subplots(figsize=self.param['figsize'])
+
+        im = ax.imshow(self.data, origin='lower',
                              extent = self.extent,
                              aspect='auto',
                              interpolation='none',
                              cmap=self.param['cmap'])
     
-        ax.set_xlabel(self.param['xlabel'])
-        ax.set_ylabel(self.param['ylabel'])
+        ax.set_xlabel(self.label['x'])
+        ax.set_ylabel(self.label['y'])
     
-        cbar = self.colorbar(im)
-        cbar.set_label(self.param['zlabel'])    
+        if not ax:
+            cbar = self.colorbar(im)
+            cbar.set_label(self.label['z'])    
 
 
 
-def plot2d_w2slices(data, h_axis, v_axis, param, hslice_pos=None, vslice_pos=None, hslice_opts=None, vslice_opts=None):
-    r"""
-    >>> uu = np.linspace(0, np.pi, 128)
-    >>> data = np.cos(uu - 0.5) * np.cos(uu.reshape(-1, 1) - 1.0)
-    >>> fig = plot2d_w2slices(data, uu, uu, {'cmap':'coolwarm',
-                                         'xlabel':r'$x$ ($\mu$m)',
-                                         'ylabel':r'$y$ ($\mu$m)',
-                                         'zlabel':r'$\rho$ (cm$^{-3}$)',
-                                         'cbar':False})
-    """
-    if not hslice_pos:
-        hslice_pos = data.shape[0] // 2 
-    if not vslice_pos:
-        vslice_pos = 9 * data.shape[1] // 10 
-    if not hslice_opts:
-        hslice_opts = {'ls': '--', 'color': '0.25'}
-    if not vslice_opts:
-        vslice_opts = hslice_opts
+    def slice(self, hslice_pos=None, vslice_pos=None, **kwargs):
+        r"""
+        >>> p2d.slice(figsize=(6.4, 6.4), hslice_pos=100, vslice_pos=10,
+                         vslice_opts={'ls':'--', 'color':'green'},
+                         hslice_opts={'ls':'-', 'color':'red'})
+        """
+        slice_opts = {'ls': '--', 'color': '0.25'}
+        hslice_opts = kwargs.get('hslice_opts', slice_opts)
+        vslice_opts = kwargs.get('vslice_opts', slice_opts)
 
-    # A lot of information about GridSpec can be found here:
-    # https://matplotlib.org/tutorials/intermediate/gridspec.html
+        if not hslice_pos:
+            hslice_pos = self.data.shape[0] // 2 
+        if not vslice_pos:
+            vslice_pos = self.data.shape[1] // 2 
+        if not vslice_opts:
+            vslice_opts = slice_opts
     
-    # Figure with three axes of different width and height. Axis-sharing
-    # is used to make easier interactive exploration but could perfectly
-    # be removed if not needed.
+        # A lot of information about GridSpec can be found here:
+        # https://matplotlib.org/tutorials/intermediate/gridspec.html
+        
+        # Figure with three axes of different width and height. Axis-sharing
+        # is used to make easier interactive exploration but could perfectly
+        # be removed if not needed.
+    
+        # set up a GridSpec grid and add the desired Axes one by one
+        self.param.update(kwargs)
 
-    # set up a GridSpec grid and add the desired Axes one by one
-    fig = plt.figure(figsize=(6.4, 6.4))
-    gs = GridSpec(2, 2, height_ratios=[1, 3], width_ratios=[3, 1])
-    ax0 = fig.add_subplot(gs[1, 0])
-    axh = fig.add_subplot(gs[0, 0], sharex=ax0)
-    axv = fig.add_subplot(gs[1, 1], sharey=ax0)
+        fig = plt.figure(figsize=self.param['figsize'])
+        gs = GridSpec(2, 2, height_ratios=[1, 3], width_ratios=[3, 1])
+        ax0 = fig.add_subplot(gs[1, 0])
+        axh = fig.add_subplot(gs[0, 0], sharex=ax0)
+        axv = fig.add_subplot(gs[1, 1], sharey=ax0)
+        
+        # Imshow global view. Some documentation about imshow:
+        # https://matplotlib.org/gallery/images_contours_and_fields/image_demo.html
+        self.cbar(ax0)
+        
+        # Just to show where the slices are done. Another example in Matplotlib
+        # gallery about the relevant functions:
+        # https://matplotlib.org/gallery/subplots_axes_and_figures/axhspan_demo.html
+        ax0.axhline(y=self.v_axis[hslice_pos], **hslice_opts)  ##----##
+        ax0.axvline(x=self.h_axis[vslice_pos], **vslice_opts)  ## | ##
+        # One can also annotate the slice markers. More information
+        # about `ax.annotate` here:
+        # https://matplotlib.org/tutorials/text/annotations.html
+        ax0.annotate('{:07.3f}'.format(self.v_axis[hslice_pos]), xy=(self.h_axis[3], self.v_axis[hslice_pos+3]), xycoords='data', color=hslice_opts['color'])
+        ax0.annotate('{:07.3f}'.format(self.h_axis[vslice_pos]), xy=(self.h_axis[vslice_pos-5], self.v_axis[self.v_axis.shape[0]//2 - 5]), xycoords='data', color=vslice_opts['color'], rotation='vertical')
+        
+        # Horizontal Slice Profile
+        axh.set_xmargin(0)  # otherwise ax0 may have white margins
+        axh.set_ylabel(self.label['z'])
+        axh.plot(self.h_axis, self.data[hslice_pos], **hslice_opts)
+        #axh.set_ylim(-1, 1)
+        #axh.set_yticks([-1, 0, 1])
+        # NB: More examples of fancy customisation of ticks and formatters here:
+        # https://matplotlib.org/gallery/ticks_and_spines/tick-locators.html
+        # https://matplotlib.org/gallery/ticks_and_spines/tick-formatters.html
+        
+        # Vertical Slice Profile
+        axv.set_ymargin(0)  # otherwise ax0 may have white margins
+        axv.set_xlabel(self.label['z'])
+        axv.plot(self.data[vslice_pos], self.v_axis, **vslice_opts)
+        #axv.set_xlim(-1, 1)
+        #axv.set_xticks([-1, 0, 1])
+        
+        # "Despine" the slice profiles and hide the relevant axis
+        for ax, spines in ((axh, ('top', 'bottom', 'right')),
+                           (axv, ('top', 'left', 'right'))):
+            for sp in spines:
+                ax.spines[sp].set_visible(False)
+        axh.xaxis.set_visible(False)
+        axv.yaxis.set_visible(False)
+        
+        # Tweak a bit the figure layout
+        fig.tight_layout()
+        fig.subplots_adjust(wspace=0.03, hspace=0.03)
     
-    # Imshow global view. Some documentation about imshow:
-    # https://matplotlib.org/gallery/images_contours_and_fields/image_demo.html
-    plot2d(ax0, data, h_axis, v_axis, param)
-    # im = ax0.imshow(data, origin='bottom', aspect='auto', cmap='coolwarm')
-    #ax0.set_ylabel('Row Index (#)')
-    #ax0.set_xlabel('Column Index (#)')
+        #return fig
+        
     
-    # Just to show where the slices are done. Another example in Matplotlib
-    # gallery about the relevant functions:
-    # https://matplotlib.org/gallery/subplots_axes_and_figures/axhspan_demo.html
-    ax0.axhline(y=v_axis[hslice_pos], **hslice_opts)  ##----##
-    ax0.axvline(x=h_axis[vslice_pos], **vslice_opts)
-    # One can also annotate the slice markers. More information
-    # about `ax.annotate` here:
-    # https://matplotlib.org/tutorials/text/annotations.html  # h_axis.shape[0]//2
-    ax0.annotate('{:07.3f}'.format(v_axis[hslice_pos]), xy=(h_axis[3], v_axis[hslice_pos+3]), xycoords='data', color=hslice_opts['color'])
-    ax0.annotate('{:07.3f}'.format(h_axis[vslice_pos]), xy=(h_axis[vslice_pos-5], v_axis[v_axis.shape[0]//2 - 5]), xycoords='data', color=hslice_opts['color'], rotation='vertical')
-    # v_axis.shape[0]//2
-    
-    # Horizontal Slice Profile
-    axh.set_xmargin(0)  # otherwise ax0 may have white margins
-    axh.set_ylabel(param['zlabel'])
-    axh.plot(h_axis, data[hslice_pos], **hslice_opts)
-    #axh.set_ylim(-1, 1)
-    #axh.set_yticks([-1, 0, 1])
-    # NB: More examples of fancy customisation of ticks and formatters here:
-    # https://matplotlib.org/gallery/ticks_and_spines/tick-locators.html
-    # https://matplotlib.org/gallery/ticks_and_spines/tick-formatters.html
-    
-    # Vertical Slice Profile
-    axv.set_ymargin(0)  # otherwise ax0 may have white margins
-    axv.set_xlabel(param['zlabel'])
-    axv.plot(data[vslice_pos], v_axis, **vslice_opts)
-    #axv.set_xlim(-1, 1)
-    #axv.set_xticks([-1, 0, 1])
-    
-    # "Despine" the slice profiles and hide the relevant axis
-    for ax, spines in ((axh, ('top', 'bottom', 'right')),
-                       (axv, ('top', 'left', 'right'))):
-        for sp in spines:
-            ax.spines[sp].set_visible(False)
-    axh.xaxis.set_visible(False)
-    axv.yaxis.set_visible(False)
-    
-    # Tweak a bit the figure layout
-    fig.tight_layout()
-    fig.subplots_adjust(wspace=0.03, hspace=0.03)
-
-    return fig
-    
-
 
 
 def plot1d_break_x(fig, h_axis, v_axis, param, slice_opts):
@@ -186,4 +187,3 @@ def plot1d_break_x(fig, h_axis, v_axis, param, slice_opts):
     kwargs.update(transform=ax_right.transAxes)  # switch to the right axes
     ax_right.plot((-d,+d), (1-d,1+d), **kwargs)
     ax_right.plot((-d,+d), (-d,+d), **kwargs)
-
